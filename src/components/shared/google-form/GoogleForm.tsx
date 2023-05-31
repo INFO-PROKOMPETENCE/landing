a0520dd4534b565dcd1ba2811b3864e831c6af1f
dimasikBurdin <mityabur@gmail.com>
2023-06-01 00:12:41 +0500

    View: micro fixic

--- src/components/shared/google-form/GoogleForm.tsx
+++ src/components/shared/google-form/GoogleForm.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
+import settings from "../../../settings/settings.json";
 import styles from "./GoogleForm.module.scss";
-import settings from "../../../settings/settings.json"
 
 interface Props {}
 
 export const GoogleForm: FC<Props> = () => {
   return (
-    <iframe className={styles.googleForm}
-            src={`https://docs.google.com/forms/d/e/${settings.googleFormId}/viewform?embedded=true`}>
-    </iframe>
+    <iframe
+      title="google-form"
+      className={styles.googleForm}
+      src={`https://docs.google.com/forms/d/e/${settings.googleFormId}/viewform?embedded=true`}
+    />
   );
 };
